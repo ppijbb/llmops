d4c18d1c75561bea5086d8f3a19e77824bfd3754
ppijbb <ppijbb@naver.com>
2024-07-31 17:11:32 +0900

    chore: Update Dockerfile to include git dependency
    The Dockerfile for the Inferentia environment setup has been updated to include the `git` dependency. This allows for version control and easy access to the repository's code within the container.
    
    Co-authored-by: [Author Name]

--- inferentia/deploy.py
+++ inferentia/deploy.py
@@ -1,16 +1,31 @@
-from optimum.neuronx import NeuronModelForCausalLM
+from fastapi import FastAPI
 
 
-# 컴파일을 위한 인자 설정
-compiler_args = {"num_cores": 2, "auto_cast_type": 'bf16'}
-input_shapes = {"batch_size": 1, "sequence_length": 4096}
 
-# 사전학습한 모델을 불러오고 설정한 값에 따라 컴파일을 수행 요청
-model = NeuronModelForCausalLM.from_pretrained(
-    model_id="finetuned-model-name-or-path",
-    export=True,
-    **compiler_args,
-    **input_shapes
-)
-# 컴파일된 모델을 저장
-model.save_pretrained("compiled-model-name-or-path")
+app = FastAPI(title="dialog summary")
+
+
+def main_f():
+
+    from optimum.neuron import NeuronModelForCausalLM
+
+
+    # 컴파일을 위한 인자 설정
+    compiler_args = {"num_cores": 2, "auto_cast_type": 'bf16'}
+    input_shapes = {"batch_size": 1, "sequence_length": 4096}
+
+    # 사전학습한 모델을 불러오고 설정한 값에 따라 컴파일을 수행 요청
+    model = NeuronModelForCausalLM.from_pretrained(
+        model_id="finetuned-model-name-or-path",
+        export=True,
+        **compiler_args,
+        **input_shapes
+    )
+    # 컴파일된 모델을 저장
+    model.save_pretrained("compiled-model-name-or-path")
+
+if __name__ == "__main__":
+    import uvicorn 
+
+    uvicorn.run(app, host="0.0.0.0", port=8000, log_level="info",)
+    
