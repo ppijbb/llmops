243f6c69b029d39ea42b1ee35028ef18b121eac6
ppijbb <ppijbb@naver.com>
2024-08-02 14:39:20 +0900

    Update LLMService model_path to use CPU optimized model

--- summary/application/engine.py
+++ summary/application/engine.py
@@ -1,8 +1,12 @@
 import torch
 import ray
 import psutil
+from typing import List
 import numpy as np
+from transformers import GenerationConfig
+
 from summary.depend import get_model
+
 
 # ray.init(
 #     num_cpus=psutil.cpu_count(logical=True), 
@@ -10,45 +14,60 @@ from summary.depend import get_model
 #     )
 
 
-def get_prompt(user_input: str, 
-               chat_history: list[tuple[str, str]],
-               system_prompt: str) -> str:
-    prompt_texts = [f'<|begin_of_text|>']
-    chat_template = '<|start_header_id|>{role}<|end_header_id|>\n\n{prompt}<|eot_id|>\n\n'
-    generate_template = f'{chat_template}<|start_header_id|>assistant<|end_header_id|>\n\n'
-    
-    def template_dict(role, prompt):
-        return { "role": role, "prompt": prompt }
-    
-    if system_prompt != '':
-        prompt_texts.append(chat_template.format(role="system", prompt=system_prompt))
-        # prompt_texts.append(template_dict(role="system", prompt=system_prompt))
-
-    for history_input, history_response in chat_history:
-        prompt_texts.append(chat_template.format(role="user", prompt=history_input.strip()))
-        # prompt_texts.append(template_dict(role="user", prompt=history_input.strip()))
-        prompt_texts.append(chat_template.format(role="assistant", prompt=history_response.strip()))
-        # prompt_texts.append(template_dict(role="assistant", prompt=history_response.strip()))
-
-    prompt_texts.append(generate_template.format(role="user", prompt=user_input.strip()))
-    # prompt_texts.append(template_dict(role="user", prompt=history_input.strip()))
-
-    return "".join(prompt_texts) if isinstance(prompt_texts[0], str) else prompt_texts
-    # return user_input
-
 # @ray.remote
 class LLMService(object):
+    default_bos: str = "<|begin_of_text|>"
+    default_eot: str = "<|eot_id|>"
+    default_start_header: str = "<|start_header_id|>"
+    default_end_header: str = "<|end_header_id|>"
+
     def __init__(self):
         self.model, self.tokenizer = get_model(
-            model_path="meta-llama/Meta-Llama-3-8B",  # GPU (vllm) Model
-            # model_path="fakezeta/llama-3-8b-instruct-ov-int8", # CPU Model
-            adapter_path=None)
+            # model_path="meta-llama/Meta-Llama-3-8B",  # GPU (vllm) Model
+            model_path="fakezeta/llama-3-8b-instruct-ov-int8", # CPU Model
+            adapter_path=None,
+            inference_tool="ov")
         
-    def formatting(self, prompt: str):
+        self.bos_token = self.tokenizer.bos_token if self.tokenizer.bos_token else self.default_bos
+        self.eot_token = self.tokenizer.eos_token if self.tokenizer.eos_token else self.default_eot
+        self.start_header = self.default_start_header
+        self.end_header = self.default_end_header
+
+    def _template_header(self, role:str = "role") -> str:
+        return f'{self.start_header}{role}{self.end_header}\n\n'
+
+    def get_prompt(self,
+                   user_input: str, 
+                   chat_history: list[tuple[str, str]],
+                   system_prompt: str = "") -> str:
+        prompt_texts = [f"{self.bos_token}"]
+        chat_template = self._template_header() + '{prompt}' + self.eot_token +'\n\n'
+        generate_template = chat_template + self._template_header(role="assistant")
+        
+        def template_dict(role, prompt):
+            return { "role": role, "prompt": prompt }
+        
+        if system_prompt != '':
+            prompt_texts.append(chat_template.format(role="system", prompt=system_prompt))
+            # prompt_texts.append(template_dict(role="system", prompt=system_prompt))
+
+        for history_input, history_response in chat_history:
+            prompt_texts.append(chat_template.format(role="user", prompt=history_input.strip()))
+            # prompt_texts.append(template_dict(role="user", prompt=history_input.strip()))
+            prompt_texts.append(chat_template.format(role="assistant", prompt=history_response.strip()))
+            # prompt_texts.append(template_dict(role="assistant", prompt=history_response.strip()))
+
+        prompt_texts.append(chat_template.format(role="user", prompt=user_input.strip()))
+        # prompt_texts.append(template_dict(role="user", prompt=user_input.strip()))
+
+        return "".join(prompt_texts) if not isinstance(prompt_texts[0], dict) else prompt_texts
+    
+
+    def formatting(self, prompt: List[str] | List[dict]):
         if isinstance(prompt, str):
             return self.tokenizer(prompt, return_tensors="pt")
         else:
-            return self.tokenizer.apply_chat_template(prompt, return_tensors="pt")
+            return { "input_ids":self.tokenizer.apply_chat_template(prompt, return_tensors="pt") }
 
     @torch.inference_mode()
     def _make_summary(self,
@@ -56,20 +75,22 @@ class LLMService(object):
         chat_template = {
             "user_input": input_text,
             "chat_history": [],
-            "system_prompt": ""
+            "system_prompt": "summarize dialogue. your job is summarizing dialouge in korean language. summarize in 3 sentecens."
         }
-        prompt = get_prompt(**chat_template)
-        if torch.cuda.is_available():
-            output_str = self.model.generate(prompt)[0].outputs[0].text
-        else:
-            inputs = self.formatting(prompt)
+        prompt = self.get_prompt(**chat_template)
+        if torch.cuda.is_available(): # vllm generation
+            output = self.model.generate(prompt)
+            output_str= output[0].outputs[0].text
+        else: # ipex, ov generation
+            inputs = self.formatting(prompt=prompt)
             output = self.model.generate(**inputs,
-                                         use_cache=True,
-                                         num_return_sequences=1,
-                                         max_new_tokens=100)
+                                         do_sample=True,
+                                        #  temperature=0.6,
+                                         max_length=4096,
+                                         top_p=0.9,
+                                         use_cache=True,)
             output_str = self.tokenizer.decode(output[0], skip_special_tokens=True)
-            print(output_str)
-        return output_str
+        return output_str.replace(". ", ".\n")
     
     @torch.inference_mode()
     def generate_stream(self, prompt, max_length=50):
@@ -86,7 +107,6 @@ class LLMService(object):
             input_ids = np.concatenate([input_ids, [[next_token]]], axis=-1)
             
             yield self.tokenizer.decode([next_token], skip_special_tokens=True)
-
 
         
     def summarize(self, input_text: str) -> str:
--- summary_serv.py
+++ summary_serv.py
@@ -1,3 +1,21 @@
+import os
+
+os.environ["CUDA_VISIBLE_DEVICES"] = ""
+os.environ['PYTORCH_ENABLE_MPS_FALLBACK'] = "0"
+# os.environ["VLLM_CPU_KVCACHE_SPACE"] = "40"
+# os.environ["VLLM_CPU_OMP_THREADS_BIND"] = "0-29"
+# os.environ['PYTORCH_ENABLE_MPS_FALLBACK'] = "0"
+# os.environ["OMP_NUM_THREADS"] = "2"
+# os.environ["ENABLE_SDP_FUSION"] = "1"
+# os.environ["SYCL_CACHE_PERSISTENT"] = "1"
+# os.environ["SYCL_PI_LEVEL_ZERO_USE_IMMEDIATE_COMMANDLISTS"] = "1"
+# os.environ["MKL_NUM_THREADS"] = "24"
+# os.environ["KMP_BLOCKTIME"] = "1"
+# os.environ["KMP_AFFINITY"] = "granularity=fine,compact,1,0"
+# os.environ["KMP_SETTINGS"] = "1"
+# os.environ["KMP_DUPLICATE_LIB_OK"] = "True"
+# os.environ["DNNL_PRIMITIVE_CACHE_CAPACITY"] = "1024"
+
 from fastapi import FastAPI, Depends
 import logging
 from typing import Annotated
@@ -9,19 +27,6 @@ import traceback
 import os
 import ray
 import torch
-
-# torch.set_num_threads(2)
-os.environ['PYTORCH_ENABLE_MPS_FALLBACK'] = '0'
-# os.environ["OMP_NUM_THREADS"] = "2"
-# os.environ["ENABLE_SDP_FUSION"] = "1"
-# os.environ["SYCL_CACHE_PERSISTENT"] = "1"
-# os.environ["SYCL_PI_LEVEL_ZERO_USE_IMMEDIATE_COMMANDLISTS"] = "1"
-# os.environ["MKL_NUM_THREADS"] = "24"
-# os.environ["KMP_BLOCKTIME"] = "1"
-# os.environ["KMP_AFFINITY"] = "granularity=fine,compact,1,0"
-# os.environ["KMP_SETTINGS"] = "1"
-# os.environ["KMP_DUPLICATE_LIB_OK"] = "True"
-# os.environ["DNNL_PRIMITIVE_CACHE_CAPACITY"] = "1024"
 
 
 def format_llm_output(rlt_text):
@@ -40,13 +45,15 @@ async def summarize(request: SummaryRequest,
     # Generate predicted tokens
     try:
         # ----------------------------------- #
-        st = time.time()
+        # st = time.time()
         # result += ray.get(service.summarize.remote(ray.put(request.text)))
         result += service.summarize(request.text)
-        end = time.time()
+        print(result)
+        # end = time.time()
         # ----------------------------------- #
 
     except Exception as e:
+        print(e)
         logging.error("error" + traceback(e))
         result += "Error in summarize"
     finally:
