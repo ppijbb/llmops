722e4c00e48f2835d03416d018b333e1a844bfa0
ppijbb <ppijbb@naver.com>
2024-09-02 14:10:09 +0900

    refactor: Update summary/application/open_ai.py to increase max_tokens for better summarization

--- summary/application/open_ai.py
+++ summary/application/open_ai.py
@@ -1,15 +1,19 @@
 import os
 import openai
+import logging
+
+from summary.application.const import DEFAULT_SUMMARY_FEW_SHOT, DEFAULT_SUMMARY_SYSTEM_PROMPT
 
 
 class OpenAIService:
     def __init__(self):
-        self.client = openai.OpenAI(api_key=os.environ["OPENAI_API_KEY"])
+        self.client = openai.OpenAI(
+            api_key=os.environ["OPENAI_API_KEY"])
 
     def generate(self, input_text:str , input_prompt:str ):
         return self.client.chat.completions.create(
             model="gpt-4o-mini",
-            max_tokens=1024,
+            max_tokens=2048,
             messages=[
                 {
                     "role": "system",
@@ -22,6 +26,8 @@ class OpenAIService:
             ]
         )
 
-    async def summarize(self, input_text:str , input_prompt:str )->str:
-        result = self.generate(input_prompt=input_prompt, input_text=input_text)
+    async def summarize(self, input_text:str , input_prompt:str=None):
+        result = self.generate(
+            input_prompt=input_prompt if input_prompt else DEFAULT_SUMMARY_SYSTEM_PROMPT, 
+            input_text=input_text)
         return result.choices[0].message.content
