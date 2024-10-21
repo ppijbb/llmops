018aa851c57defd7ea2b95cb22c6ad3654e3fc22
ppijbb <ppijbb@naver.com>
2024-10-21 03:48:42 +0000

    refactor: Update Dockerfile and requirements.txt to include additional Neuron-X dependencies and PyTorch versions

--- inferentia/deploy.py
+++ inferentia/deploy.py
@@ -1,12 +1,20 @@
 from fastapi import FastAPI
 from vllm import LLM, SamplingParams
 from optimum.neuron import NeuronModelForCausalLM
+from transformers import AutoTokenizer
 import uvicorn
+import os
+import time
+
+os.environ['NEURON_VISIBLE_CORES']='2'
+os.environ['NEURON_RT_NUM_CORES']='2'
+
 
 app = FastAPI(title="dialog summary")
 
 
 def vllm():
+    print("Call VLLM Function")
     # Sample prompts.
     prompts = [
         "Hello, my name is",
@@ -29,12 +37,12 @@ def vllm():
         # The device can be automatically detected when AWS Neuron SDK is installed.
         # The device argument can be either unspecified for automated detection, or explicitly assigned.
         device="neuron",
-        tensor_parallel_size=2)
+        tensor_parallel_size=1)
     # Generate texts from the prompts. The output is a list of RequestOutput objects
     # that contain the prompt, generated text, and other information.
     outputs = llm.generate(prompts, sampling_params)
     # Print the outputs.
-    result_text = ""
+    result_text = "안녕하세요 저는 미국에서온 "
     for output in outputs:
         prompt = output.prompt
         generated_text = output.outputs[0].text
@@ -43,6 +51,7 @@ def vllm():
     return result_text
 
 def main_f():
+    print("Call Main Function")
     # 컴파일을 위한 인자 설정
     compiler_args = {
         "num_cores": 2, 
@@ -52,24 +61,43 @@ def main_f():
         "batch_size": 1, 
         "sequence_length": 128
         }
-
+    model_path = "TinyLlama/TinyLlama-1.1B-Chat-v1.0"
     # 사전학습한 모델을 불러오고 설정한 값에 따라 컴파일을 수행 요청
     model = NeuronModelForCausalLM.from_pretrained(
-        model_id="TinyLlama/TinyLlama-1.1B-Chat-v1.0",
+        model_id=model_path,
         export=True,
         **compiler_args,
-        **input_shapes
-    )
+        **input_shapes)
+    tokenizer = AutoTokenizer.from_pretrained(
+        model_path, 
+        trust_remote_code=True)
+
     # 컴파일된 모델을 저장
-    return model.config
+    start = time.time()
+    result = model.generate(
+        **tokenizer("Hello, my name is", return_tensors='pt'), 
+        max_new_tokens=512,
+        do_sample=True,
+        temperature=0.9,
+        top_k=50,
+        top_p=0.9)
+    print(tokenizer.decode(result[0]))
+    print(time.time() - start)
+    return model.config.to_dict()
+
+@app.on_event("startup")
+async def startup_event():
+    main_f()
+    # vllm()
 
 
 @app.get("/")
 def read_root():
+    print("MAIN CALLED")
     return {
         "message": "Neuron model is compiled and saved",
         "neuron_model": main_f(),
-        "text": vllm()
+        # "text": vllm()
         }
 
 # uvicorn.run(app, host="0.0.0.0", port=8000)
