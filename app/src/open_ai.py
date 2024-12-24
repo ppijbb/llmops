32ecd523a4033859db6a70592bef0a379d713ee3
ppijbb <ppijbb@naver.com>
2024-12-24 13:45:00 +0900

    feat: Implement short message responses for various target languages in OpenAIService and enhance context handling for translation summaries

--- app/src/open_ai.py
+++ app/src/open_ai.py
@@ -5,6 +5,7 @@ from typing import List
 import openai
 
 from app.src.const import prompt
+from app.enum.transcript import TargetLanguages
 
 
 class OpenAIService:
@@ -12,6 +13,16 @@ class OpenAIService:
         self.client = openai.OpenAI(
             api_key=os.environ["OPENAI_API_KEY"])
         self.logger = logging.getLogger("ray.serve")
+
+    def _short_message_for_language(self, target_language: str) -> str:
+        if target_language == TargetLanguages.get_language_name(TargetLanguages.CHINESE):
+            return "演讲太短了。"
+        elif target_language == TargetLanguages.get_language_name(TargetLanguages.ENGLISH):
+            return "Speech is too short."
+        elif target_language == TargetLanguages.get_language_name(TargetLanguages.KOREAN):
+            return "발화가 너무 짧습니다."
+        else:
+            return "Speech is too short."
 
     def generate(
         self, 
@@ -59,7 +70,7 @@ class OpenAIService:
             source=source_language,
             detect=detect_language, 
             target=target_language,
-            context=" ".join([history[-1] if len(history)>0 else "", input_text]),
+            context=" ".join([history[-1] if len(history) > 0 else "", input_text]),
             input_text=input_text)
         result = self.generate(
             input_prompt=input_prompt if input_prompt else default_system_prompt, 
@@ -92,10 +103,13 @@ class OpenAIService:
         target_language:List[str]=["en"],
         input_prompt:str=None,
     ) -> str:
-        result = self.generate(
-            input_prompt=input_prompt if input_prompt else prompt.DEFAULT_TRANSLATION_SUMMARIZE_SYSTEM_PROMPT.format(
-                # source=source_language,
-                target=target_language[0]), 
-            input_text=f'<speech>{input_text}</speech>')
-        return result.choices[0].message.content
+        if len(input_text) < 200:
+            return f"**Summary by Transera**\n- {self._short_message_for_language(target_language[0])}"
+        else:
+            result = self.generate(
+                input_prompt=input_prompt if input_prompt else prompt.DEFAULT_TRANSLATION_SUMMARIZE_SYSTEM_PROMPT.format(
+                    # source=source_language,
+                    target=target_language[0]), 
+                input_text=f'Target Language={target_language[0]}\n<speech>{input_text}</speech>\n')
+            return result.choices[0].message.content
       
